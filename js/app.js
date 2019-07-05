0cd1cd4576122029d1f87a63f5d2ac196fceea08
Chigorin M.L <mmchigorin@gmail.com>
2019-07-05 14:56:49 +0300

    add icons and show the temperature on the view

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 const cityForm = document.querySelector('.change-location');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
 
 const updateUI = (data) => { 
 
-  const cityDets = data.cityDets;
-  const weather = data.weather;
+  // const cityDets = data.cityDets;
+  // const weather = data.weather;
 
-  console.log(data);
+  //object destructuring
+  const {cityDets, weather} = data;
 
   // update details template
   details.innerHTML = `
@@ -18,9 +22,26 @@ const updateUI = (data) => {
       <span>&deg;C</span>
     </div>
   `;
+
+  //remove the d-none class if present
+  if (card.classList.contains('d-none')) { 
+    card.classList.remove('d-none');
+  }
+
+  //update the night/day & icon images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = 'img/day.svg';
+  } else { 
+    timeSrc = 'img/night.svg';
+  }
+  time.setAttribute('src', timeSrc);
 }
 
-const updateCity = async (city) => {
+const updateCity = async (city) => { 
 
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
